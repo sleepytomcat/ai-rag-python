7415e5da71a5349fb831d5936a0d84fa5cee0ebf
Sergei Semenov <sleepytomcat@gmail.com>
2026-05-06 12:18:49 -0700

    Weather report fetch

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from datasets import load_from_disk
+from dotenv import load_dotenv
+import requests
+from langchain.agents import create_agent
+from langchain.tools import tool
+
 
 
 def main():
-    ds = load_from_disk(".wikipedia-dataset")
-    for entry in ds.select([3,5,7]):
-        print(entry)
+    load_dotenv()
+    print(get_weather("San Francisco"))
+    pass
+
+
+#@tool('get_weather', description='get weather in a given city', return_direct=True)
+def get_weather(city: str):
+    response = requests.get(f'https://wttr.in/{city}')
+    return response.text
 
 
 if __name__ == "__main__":
--- scripts/test_gemini_access.py
+++ scripts/test_gemini_access.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google.genai import Client
 
+
 load_dotenv()
 
 for key, value in os.environ.items():

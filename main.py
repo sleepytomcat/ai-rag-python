2fc6ba8239e8e8f2bd245e544265123e338281df
Sergei Semenov <sleepytomcat@gmail.com>
2026-05-07 16:47:24 -0700

    Conversation

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import requests
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 from langchain.tools import tool
 
 
 def main():
     load_dotenv()
 
-    print("> Chat model call")
+    conversation = [
+        SystemMessage("You are a helpful weather agent who also adds a joke to replies."),
+        HumanMessage("What is the weather now in San Francisco?"),
+        AIMessage("As of right now in San Francisco, the weather is partly cloudy with a temperature of around 62°F."),
+        HumanMessage("Is it warmer in Vienna now?")
+    ]
+
     chat_model = init_chat_model(
         model="gemini-3.1-flash-lite",
         model_provider="google_genai",
@@ -18,25 +25,10 @@ def main():
     )
 
     t0 = time.perf_counter()
-    response = chat_model.invoke("What is the weather in San Francisco now? Would you recommend to have an umbrella?")
-    print(f"chat_model.invoke() took {time.perf_counter() - t0:.2f}s")
+    response = chat_model.invoke(conversation)
+    print(f"chat_model.invoke(conversation) took {time.perf_counter() - t0:.2f}s")
 
-    print(response.text)
-
-    print("> Agent call")
-    agent = create_agent(model=chat_model, tools=[get_weather], system_prompt="You are a helpful weather agent who also adds a dark joke to replies.")
-
-    t0 = time.perf_counter()
-    response = agent.invoke(
-        {
-            'messages': [
-                {"role":"user", "content": "What is the weather in Vienna now?"}
-            ]
-        }
-    )
-    print(f"agent.invoke() took {time.perf_counter() - t0:.2f}s")
-
-    print(response['messages'][-1])
+    print(response.content)
 
 @tool('get_weather', description='get weather in a given city', return_direct=False)
 def get_weather(city: str):
